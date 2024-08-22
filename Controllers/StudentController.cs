ba08ee36ea50506580bdefc22ea96c7897f893ca
HazemAmayreh1 <hazemamayreh44@gmail.com>
2024-08-22 16:07:35 +0300

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace studentController.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
